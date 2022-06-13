3e0c044bde630940286c6936db9f02a8fadcee91
mike23101994 <68069497+mike23101994@users.noreply.github.com>
2022-06-13 16:24:08 +0530

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -7,6 +7,11 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_ami_from_instance" "example" {
+  name               = "terraform-example"
+  source_instance_id = aws_instance.web.id
+}
+
 
 terraform {
   backend "s3" {
